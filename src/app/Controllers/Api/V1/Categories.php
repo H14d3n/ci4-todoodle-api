206060d8fef4fb1b085343d7c28b685a013ce6f6
Tizian Imseng <tizian.imseng@bluewin.ch>
2025-05-07 14:44:47 +0200

    [Add] Functions to CRUD Todos and Categories
    + Create
    + Read
    + Update
    + Delete

--- src/app/Controllers/Api/V1/Categories.php
+++ src/app/Controllers/Api/V1/Categories.php
@@ -14,4 +14,41 @@ class Categories extends ResourceController
     {
         return $this->respond($this->model->findAll());
     }
+
+    public function show($id = null)
+    {
+        $category = $this->model->find($id);
+        if ($category) {
+            return $this->respond($category);
+        } else {
+            return $this->failNotFound('Category not found');
+        }
+    }
+    public function create()
+    {
+        $data = $this->request->getPost();
+        if ($this->model->insert($data)) {
+            return $this->respondCreated($data);
+        } else {
+            return $this->failValidationErrors($this->model->errors());
+        }
+    }
+    public function update($id = null)
+    {
+        $data = $this->request->getRawInput();
+        if ($this->model->update($id, $data)) {
+            return $this->respondUpdated($data);
+        } else {
+            return $this->failValidationErrors($this->model->errors());
+        }
+    }
+
+    public function delete($id = null)
+    {
+        if ($this->model->delete($id)) {
+            return $this->respondDeleted(['id' => $id]);
+        } else {
+            return $this->failNotFound('Category not found');
+        }
+    }
 }
--- src/app/Controllers/Api/V1/Todos.php
+++ src/app/Controllers/Api/V1/Todos.php
@@ -14,4 +14,43 @@ class Todos extends ResourceController
     {
         return $this->respond($this->model->findAll());
     }
+
+    public function show($id = null)
+    {
+        $todo = $this->model->find($id);
+        if ($todo) {
+            return $this->respond($todo);
+        } else {
+            return $this->failNotFound('Todo not found');
+        }
+    }
+
+    public function create()
+    {
+        $data = $this->request->getPost();
+        if ($this->model->insert($data)) {
+            return $this->respondCreated($data);
+        } else {
+            return $this->failValidationErrors($this->model->errors());
+        }
+    }
+
+    public function update($id = null)
+    {
+        $data = $this->request->getRawInput();
+        if ($this->model->update($id, $data)) {
+            return $this->respondUpdated($data);
+        } else {
+            return $this->failValidationErrors($this->model->errors());
+        }
+    }
+
+    public function delete($id = null)
+    {
+        if ($this->model->delete($id)) {
+            return $this->respondDeleted(['id' => $id]);
+        } else {
+            return $this->failNotFound('Todo not found');
+        }
+    }
 }
